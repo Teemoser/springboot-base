ea426bab89af72e05027a4a727be95da134020cf
吴晓津 <wuxj@deeplypet.com>
2019-04-28 19:23:08 +0800

    update package name

---
+++ src/test/java/com/teemo/DeeplypetApplicationTests.java
@@ -0,0 +1,17 @@
+package com.teemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeeplypetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		// unit test
+	}
+
+}
